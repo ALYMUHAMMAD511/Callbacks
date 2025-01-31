7a2dd09359c1ef780a95a8693c16bf1e110855d2
alymu <alym7mdd@gmail.com>
2025-02-01 01:37:48 +0200

    More callbacks

--- app/src/main/java/com/example/callbacks/MainActivity.kt
+++ app/src/main/java/com/example/callbacks/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.widget.addTextChangedListener
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editText: EditText
     private lateinit var switch: SwitchMaterial
     private lateinit var button: Button
+    private lateinit var textView: TextView
+    private lateinit var imageView: ImageView
 
     companion object {
         const val LOG_TAG = "MyActivity"
@@ -37,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.edit_text)
         switch = findViewById(R.id.switch_)
         button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+        imageView = findViewById(R.id.imageView)
     }
 
     private fun addCallbacks() {
@@ -47,6 +54,25 @@ class MainActivity : AppCompatActivity() {
         editText.addTextChangedListener { text ->
             Log.d(LOG_TAG, text.toString())
         }
+        button.setOnClickListener {
+            updateText()
+            updateColor()
+            updateImage()
+        }
+    }
+
+    private fun updateText(){
+        textView.text = getString(R.string.aly)
+    }
+
+    private fun updateColor(){
+        val red = ContextCompat.getColor(this, R.color.red)
+        textView.setTextColor(red)
+    }
+
+    private fun updateImage(){
+        val image = ContextCompat.getDrawable(this, R.drawable.baseline_block_24)
+        imageView.setImageDrawable(image)
     }
 
 }
